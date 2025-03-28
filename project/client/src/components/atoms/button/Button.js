39d792ff5ddcb019f630586fe79cd625a9b2cc35
Amelia <nawrotamelia3@gmail.com>
2025-03-29 00:41:44 +0100

    local

---
+++ project/client/src/components/atoms/button/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Button.css";
+
+
+const Button =({text})=>{
+
+    return(
+        <>
+            <div className="btn">{text}</div>
+        </>
+    );
+
+}
+
+export default Button;
